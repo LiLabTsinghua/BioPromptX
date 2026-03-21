dec4190d73dc02ed81a47efe9061053bbf330c45
Zhetao GUO <71634171+guozhetao@users.noreply.github.com>
2026-03-21 16:45:06 +0800

    Update qwen_api.py

--- Code/Gene_function/get_EC/qwen_api.py
+++ Code/Gene_function/get_EC/qwen_api.py
@@ -3,15 +3,15 @@ import time
 import sentencepiece
 
 def countTokens(prompt):
-    sp = sentencepiece.SentencePieceProcessor(model_file='/home/xiongwen/llama2-model/llama2-13b-chat/tokenizer.model')
+    sp = sentencepiece.SentencePieceProcessor(model_file='/home/zhetao/.cache/modelscope/hub/AI-ModelScope/Mixtral-8x7B-Instruct-v0.1/tokenizer.model')
     prompt_tokens = sp.encode_as_ids(prompt)
     return len(prompt_tokens)
 
 def qwen_api(user_message, port=8000, top_p=0.9, temperature=0.5, system_message='', max_tokens=4096):
-    # 初始化 OpenAI 客户端
+    # OpenAI
     client = OpenAI(
-        base_url=f"http://localhost:{port}/v1",  # 设置 API 的基础 URL
-        api_key="none"  # 设置 API 密钥
+        base_url=f"http://localhost:{port}/v1",  # API URL
+        api_key="none"  # API key
     )
 
     history = []
@@ -20,30 +20,32 @@ def qwen_api(user_message, port=8000, top_p=0.9, temperature=0.5, system_message
     history.append({"role": "user", "content": user_message})
 
     try:
-        # 创建请求
+        # Ask
         start_time = time.time()
         response = client.chat.completions.create(
-            model="/home/zhetao/.cache/modelscope/hub/LLM-Research/Llama-3.3-70B-Instruct",  # 模型路径
+            # model="gpt-oss-120b",  # model path
+            # model="Llama3.1_8B_NER",
+            model="Llama-3.3-70B-Instruct",
             messages=history,
             max_tokens=max_tokens,
             temperature=temperature,
             top_p=top_p,
             frequency_penalty=0.0,
             presence_penalty=1.0,
-            stop=[],  # 可以添加自定义的停止词
+            stop=[],
             stream=False
         )
         use_time = time.time() - start_time
         response_content = response.choices[0].message.content
         print(response_content)
         token_length = countTokens(response_content)
-        print('花费时间:', round(use_time, 2), '秒\n速率:', round(token_length/use_time, 2), 'token/s')
+        print('time cost:', round(use_time, 2), 'Second\nSpeed:', round(token_length/use_time, 2), 'token/s')
 
     except KeyError as e:
-        print(f"KeyError: {e} - 可能是因为响应结构不符合预期。")
-        print(f"完整响应内容: {response if 'response' in locals() else '没有收到有效响应'}")
+        print(f"KeyError: {e} - The response structure does not match expectations")
+        print(f"Full responce: {response if 'response' in locals() else 'No responds'}")
     except Exception as ex:
-        print(f"其他错误: {ex}")
+        print(f"Other errors: {ex}")
 
     return response_content if 'response_content' in locals() else None
 
