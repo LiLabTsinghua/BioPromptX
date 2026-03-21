83ecb697f0bb7cc379aed0aed4f14dd278fff8f4
Zhetao GUO <71634171+guozhetao@users.noreply.github.com>
2026-03-21 16:51:20 +0800

    Update get_pubmed_gene_number.py

--- Code/Gene_function/get_gene/get_pubmed_gene_number.py
+++ Code/Gene_function/get_gene/get_pubmed_gene_number.py
@@ -1,26 +1,26 @@
 import json
 import pandas as pd
 
-# 读取 JSON 文件
+# Read JSON
 with open("/home/zhetao/Human_gene/Info_Extration/gene_function/sliced200_amino_acid_paper_gene_combine.json", "r", encoding="utf-8") as f:
     data = json.load(f)
 
 
-# 统计每篇文章中的基因数量并合并基因名字
+# Count the number of genes in each article and combine the gene names
 records = []
 for article in data:
     pubmed_id = article.get("PubMed_ID", "").strip()
     genes = article.get("Genes", [])
     gene_count = len(genes)
-    genes_str = ", ".join(genes)  # 用逗号连接所有基因名
+    genes_str = ", ".join(genes) 
     records.append({
         "PubMed_ID": pubmed_id,
         "Gene_Count": gene_count,
         "Genes": genes_str
     })
 
-# 保存到 Excel 文件
+# Save
 df = pd.DataFrame(records)
 df.to_excel("gene_count_amino_acid.xlsx", index=False)
 
-print("统计结果已保存为 gene_counts_per_article.xlsx")
+print("save as gene_counts_per_article.xlsx")
